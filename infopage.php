62fa086bed7a91bd0eaca9af6783be2890539706
stijn versluis <stijn.versluis@gmail.com>
2019-09-02 11:31:58 +0200

    addinfopage

---
+++ infopage.php
@@ -0,0 +1,59 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <title>Info - 10xhooghouden</title>
+</head>
+<body>
+
+<header>
+
+    <?php include 'header.php';?>
+
+</header>
+
+<main>
+
+    <div class="infopage">
+
+        <div class="Infopage-Menu">
+
+            <ul>
+                <li><a class="active" href="#home">Home</a></li>
+                <li><a href="#game">Game</a></li>
+                <li><a href="#info">Info</a></li>
+                <li><a href="#leaderboard">Leaderboard</a></li>
+            </ul>
+
+        </div>
+
+        <div class="Infopage-Main">
+
+            <h1>Informatie over het Speel en de Ontwikkelaars</h1>
+
+            <p>Hallo, welkom hier! Wij zijn studenten van de Radius College en wij zijn hier met een doel.
+               Een leuk speel creëren voor de gebruikers! Wij hopen dat jullie leuk tijden hier hebben!</p>
+
+            <p>Als u hulp nodig heeft met iets, vraag gerust aan ons via mondeling of deze link hieronder.</p>
+
+            <a href="#infolink">Info Link</a>
+
+        </div>
+
+    </div>
+
+</main>
+
+<footer>
+
+    <?php include 'footer.php';?>
+
+</footer>
+
+</body>
+</html>
