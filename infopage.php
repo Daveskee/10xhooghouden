8e0fdaf9409f488dc6246b148f9e5d168cda9b96
stijn versluis <stijn.versluis@gmail.com>
2019-09-02 11:42:20 +0200

    infopage

--- infopage.php
+++ infopage.php
@@ -1,36 +1,9 @@
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    <title>Info - 10xhooghouden</title>
-</head>
-<body>
-
-<header>
 
     <?php include 'header.php';?>
 
-</header>
-
-<main>
 
     <div class="infopage">
-
-        <div class="Infopage-Menu">
-
-            <ul>
-                <li><a class="active" href="#home">Home</a></li>
-                <li><a href="#game">Game</a></li>
-                <li><a href="#info">Info</a></li>
-                <li><a href="#leaderboard">Leaderboard</a></li>
-            </ul>
-
-        </div>
 
         <div class="Infopage-Main">
 
@@ -46,14 +19,4 @@
         </div>
 
     </div>
-
-</main>
-
-<footer>
-
     <?php include 'footer.php';?>
-
-</footer>
-
-</body>
-</html>
