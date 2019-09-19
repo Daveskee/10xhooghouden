4f7c44e1dced40d2cb852fe03b24fbfea097f78d
Dave van Oosterhout <djavo@live.nl>
2019-09-19 09:55:10 +0200

    Update info-page

--- infopage.php
+++ infopage.php
@@ -11,23 +11,23 @@ require __DIR__ . '/header.php';?>
 
                 <div class="persoon perpos1">
                     <h2>Stijn</h2>
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab deleniti dignissimos minus molestiae quidem ullam. Adipisci architecto debitis deleniti dolorem et harum, ipsam obcaecati perspiciatis quae quas qui, ut voluptatem.</p>
                 </div>
                 <div class="persoon perpos2">
                     <h2>Martin</h2>
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquid amet dignissimos, doloremque dolores eius eos illum ipsam molestiae neque obcaecati optio placeat qui quisquam recusandae suscipit tempora ullam veniam!</p>
                 </div>
                 <div class="persoon perpos3">
                     <h2>Dave</h2>
-                    <p></p>
+                    <p>Ik vond het idee om zelf alles te bedenken in het begin een beetje apart want je had dus helemaal niks om op te bouwen, maar toen we gingen brainstormen kwam er een heel leuk idee op uit en dat is dit geworden.</p>
                 </div>
                 <div class="persoon perpos4">
                     <h2>Falco</h2>
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis consequuntur corporis culpa error et explicabo id incidunt, ipsum iste maiores molestiae molestias numquam possimus quidem suscipit tempora, veniam vitae!</p>
                 </div>
                 <div class="persoon perpos5">
                     <h2>Kane</h2>
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eveniet expedita magni nihil quisquam quod rem. Atque dignissimos esse, ex, excepturi fugiat harum, impedit incidunt iure minima ratione sint voluptas!</p>
                 </div>
 
             </div>
